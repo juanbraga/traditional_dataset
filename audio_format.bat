8a11144bae9aa4960740f75f2e9cdd28c2b10a1a
Juan Braga <juanbragabrum@gmail.com>
2016-02-16 16:48:09 -0300

    batch file updated. density fragments formated to mono, pcm, 16bits, 44100 audio files.

--- audio_format.bat
+++ audio_format.bat
@@ -1,19 +1,7 @@
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold1\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold1\%%~na.wav
+for %%a in ("./allemande/fragments/*.wav") do ffmpeg -i ./allemande/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./allemande/fragments/%%~na_mono.wav
 
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold2\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold2\%%~na.wav
+for %%a in ("./syrinx/fragments/*.wav") do ffmpeg -i ./syrinx/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./syrinx/fragments/%%~na_mono.wav
 
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold3\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold3\%%~na.wav
+for %%a in ("./density/fragments/*.wav") do ffmpeg -i ./density/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./density/fragments/%%~na_mono.wav
 
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold4\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold4\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold5\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold5\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold6\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold6\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold7\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold7\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold8\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold8\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold9\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formated\fold9\%%~na.wav
-
-for %%a in ("\\CTRIAS\audio_dataset\UrbanSound8K\audio\fold10\*.wav") do ffmpeg -i "%%a" -ar 44100 -ac 1 -sample_fmt s16 \\CTRIAS\audio_dataset\UrbanSound8K\formatedfold10\%%~na.wav
+for %%a in ("./sequenza/fragments/*.wav") do ffmpeg -i ./sequenza/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./sequenza/fragments/%%~na_mono.wav
