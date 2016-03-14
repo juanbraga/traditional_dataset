e13998e185f51631995e37ebfda4c70c8e84875b
Juan Braga <juanbragabrum@gmail.com>
2016-03-14 18:37:42 -0300

    .bat ignoring and deleted

--- audio_format.bat
+++
@@ -1,7 +0,0 @@
-for %%a in ("./allemande/fragments/*.wav") do ffmpeg -i ./allemande/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./allemande/fragments/%%~na_mono.wav
-
-for %%a in ("./syrinx/fragments/*.wav") do ffmpeg -i ./syrinx/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./syrinx/fragments/%%~na_mono.wav
-
-for %%a in ("./density/fragments/*.wav") do ffmpeg -i ./density/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./density/fragments/%%~na_mono.wav
-
-for %%a in ("./sequenza/fragments/*.wav") do ffmpeg -i ./sequenza/fragments/"%%a" -ar 44100 -ac 1 -sample_fmt s16 ./sequenza/fragments/%%~na_mono.wav
